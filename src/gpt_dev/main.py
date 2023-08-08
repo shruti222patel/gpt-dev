33452e5687a700f6bd4441ccf902802a0751b640
Shruti Patel <shruti222patel@gmail.com>
2023-08-08 17:06:05 -0400

    Now publishing comments to specified PR

--- src/gpt_dev/main.py
+++ src/gpt_dev/main.py
@@ -1,5 +1,10 @@
-from itertools import islice
+import urllib
+from collections import defaultdict
+from typing import List, Dict
 
+import requests
+from itertools import islice
+from unidiff import PatchSet, PatchedFile, Hunk
 from github import Github
 import os
 
@@ -10,70 +15,236 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 OWNER = os.environ.get('OWNER', "shruti222patel")
 REPO_NAME = os.environ.get('REPO_NAME', "repo-gpt")
-BASE_URL = os.environ.get('BASE_URL', "https://github.com")
+GIT_HOSTNAME = os.environ.get('GIT_HOSTNAME', "github.com")
+BASE_URL = 'https://api.github.com'
 
 IGNORE_FILE_EXTENTIONS = [".ipynb", ".lock"]
 
 # Authenticate with the provided token
-g = Github(GITHUB_TOKEN)
+g = Github(GITHUB_TOKEN) # Doesn't work: base_url=f"https://{GIT_HOSTNAME}/api/v3"
 # Create Openai service
 openai_service = OpenAIService(OPENAI_API_KEY)
 
 # Get the repository
 repo = g.get_repo(f"{OWNER}/{REPO_NAME}")
 
-
-def extract_final_code_from_patch(patch):
-    """Extracts final code from a git patch by removing lines starting with '-'
-    and stripping leading '+' from added lines.
-
-    Args:
-    - patch (str): The git diff patch text.
-
-    Returns:
-    - str: The final code after applying changes from the patch.
-    """
-
-    # Split the patch into lines
-    lines = patch.split('\n')
-
-    # Process each line
-    processed_lines = []
-    for line in lines:
-        if line.startswith('-'):
-            # Ignore removed lines
-            continue
-        # Strip the leading '+' and ' ', then add to the result
-        processed_lines.append(line[1:])
-
-    return '\n'.join(processed_lines)
-
-def get_pr_diff(pull_number):
-
+def get_all_pr_comments(pull_number):
     # Get the pull request
     pull = repo.get_pull(pull_number)
 
+    # Fetch the comments
+    comments = list(pull.get_comments())
+    for comment in comments:
+        print(comment)
+        print(f"commit_id: {comment.commit_id}")
+
+def get_pr_diff_summary(pull_number):
+
+    # Get the pull request
+    pull = repo.get_pull(pull_number)
+    github_patch_processor = GitDiffProcessor(pull.diff_url)
+    head_sha = pull.head.sha
     # Fetch the diff
-    files = list(islice(pull.get_files(), 51))
+    files = list(islice(pull.get_files(), 51)) # only pulls the first 50 files updated in the PR
     comments = {}
     for file in files:
         path = file.filename
         patch = file.patch
-        # contents = repo.get_contents(path, ref=head_sha)
-        # content = contents.decoded_content.decode()
+        contents = repo.get_contents(path, ref=head_sha)
+        content = contents.decoded_content.decode()
         if os.path.splitext(path)[1] not in IGNORE_FILE_EXTENTIONS and patch:
-            print("-----------------------------------------------")
-            updated_code = extract_final_code_from_patch(patch)
-            print(updated_code)
-            print("-------")
-            comment = openai_service.analyze(path, updated_code)
-            print(comment)
-            comments[path] = comment
+            # print("-----------------------------------------------")
+            # print(f"Path: {path}")
+            # print(f"Patch: {patch}")
 
-    summary = openai_service.summarize(comments.values())
+            updated_code = github_patch_processor.get_openai_code_output(path)
+            # print(updated_code)
+            # print("-------")
+            comment = openai_service.analyze(updated_code, content)
+            # print(comment)
+
+            if "no suggestions" not in comment.lower():
+                comments[path] = comment
+
+                # code_suggestions = openai_service.code_suggestions(updated_code, comment)
+                # print(code_suggestions)
+                # create_review_comment(pull_number, path, code_suggestions)
+                # post_github_comment(pull_number, "test", path, 31)
+            # get_all_pr_comments(pull_number)
+            # post_github_comment(pull_number, "test", path, 31)
+            # get_all_pr_comments(pull_number)
+            #
+            # break
+
+    summary = openai_service.summarize(comments)
 
     return summary
 
 
-diff = get_pr_diff(9)
-print(diff)
+class GitCodeSuggestion:
+    def __init__(self, hunk: Hunk):
+        self.hunk = hunk
+
+    def check(self):
+        return self.hunk.is_valid()
+
+    def get_git_start_line(self):
+        return self.hunk.source_start
+
+    def get_git_end_line(self):
+        return self.hunk.source_start + self.hunk.source_length
+
+    def get_git_code(self):
+        return "```suggestion\n"+ ''.join(line for line in self.hunk)+"\n```"
+
+class GitDiffProcessor:
+    def __init__(self, diff_url: str):
+        diff = urllib.request.urlopen(diff_url)
+        encoding = diff.headers.get_charsets()[0]
+        self.patch = PatchSet(diff, encoding=encoding)
+        self.target_code = self.get_target_code_output()
+
+    def get_target_code_output(self) -> Dict[str, List[str]]:
+        output = defaultdict(list)
+        for patched_file in self.patch:
+            for hunk in patched_file:
+                if hunk.is_valid():
+                    output[patched_file.path].append(hunk.target)
+                else:
+                    print("Invalid hunk")
+                    print(hunk)
+        return output
+
+
+    def get_openai_code_output(self, filename:str) -> List[str]:
+        prompt = ""
+        for code_snippet in self.target_code[filename]:
+            prompt_substring = []
+            for line in code_snippet:
+                if line.startswith("+"):
+                    prompt_substring.append(f" {line[1:]}")
+                else:
+                    prompt_substring.append(f"{line}")
+            prompt += f"```\n...other code\n{''.join(prompt_substring)}...other code\n```\n"
+        return prompt
+
+
+
+
+
+class ProcessOpenAIDiff:
+    def __init__(self, openai_response: str):
+        self.diff, self.explanation = self.create_unidiff(openai_response)
+
+    def create_unidiff(self, openai_resonse: str) -> (PatchedFile, str):
+        """Create a unidiff from the openai response.
+
+        Args:
+        - openai_response (str): The openai response.
+
+        Returns:
+        - str: The unidiff.
+        """
+
+        # Split the response into lines
+        lines = openai_resonse.split('\n')
+
+        CODE_DENOTER = '```'
+        # Process each line
+        inside_code_block = False
+        code_diff_lines = []
+        text_explanation_lines = []
+        for line in lines:
+            if line.startswith(CODE_DENOTER):
+                inside_code_block = not inside_code_block
+                continue
+            if inside_code_block:
+                code_diff_lines.append(line)
+            else:
+                text_explanation_lines.append(line)
+
+        patch = PatchSet(code_diff_lines)
+        if len(patch) == 0:
+            raise ValueError("Expected at least one file in the diff, but got none.")
+        elif len(patch) > 1:
+            print(
+                "Expected only one file in the diff, but got more than 1. Only using the diff from the first file in the patch.")
+
+        return patch[0], '\n'.join(text_explanation_lines)
+
+    def get_git_code_suggestions(self) -> List[GitCodeSuggestion]:
+        """Get the git code suggestions from the openai response.
+
+        Returns:
+        - List[GitCodeSuggestion]: The git code suggestions.
+        """
+
+        return [GitCodeSuggestion(hunk) for hunk in self.diff]
+
+
+
+
+
+
+def create_review_comment(pull_number, comment):
+    """Creates a review comment on the given pull request.
+
+    Args:
+    - pull_number (int): The pull request number.
+    - comment (str): The comment to post.
+    """
+
+    # Get the pull request
+    pull = repo.get_pull(pull_number)
+
+    # pull.create_issue_comment(comment)
+    # Post the comment
+    commit_id = pull.get_commits().reversed[0]
+    print(f"commit_id: {commit_id}")
+    print(f"type: {type(commit_id)}")
+    pull.create_issue_comment(comment)
+
+
+def get_latest_commit_sha(pull_number):
+    pull = repo.get_pull(pull_number)
+    return pull.get_commits().reversed[0].sha
+
+def post_github_comment(pull_number, body, path, start_line, end_line=None):
+    print(f"Posting comment on PR #{pull_number} on {path} at line {start_line}")
+    print(f"path type: {type(path)}")
+
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    end_line = end_line or start_line + 1
+    data = {
+        'body': body,
+        'commit_id': get_latest_commit_sha(pull_number),
+        'path': path,
+        'start_line': start_line,
+        'start_side': 'RIGHT',
+        'line': end_line,
+        'side': 'RIGHT'
+    }
+
+    response = requests.post(f'{BASE_URL}/repos/{OWNER}/{REPO_NAME}/pulls/{pull_number}/comments', headers=headers,
+                             json=data)
+
+    if response.status_code == 201:
+        print("Comment added successfully!")
+    else:
+        print(f"Failed to add comment! Status code: {response.status_code}, Response: {response.text}")
+
+    return response
+
+if __name__ == '__main__':
+    pull_number = 9
+    summary = get_pr_diff_summary(pull_number)
+
+    if "no comments" not in summary.lower():
+        print(f"Summary: {summary}")
+        # save summary as an issue comment
+        create_review_comment(pull_number, summary)
--- src/gpt_dev/openai_service.py
+++ src/gpt_dev/openai_service.py
@@ -1,7 +1,7 @@
 # Set your OpenAI API key as an environment variable
 import os
 import time
-from typing import Any, Callable
+from typing import Any, Callable, Dict
 
 import openai as openai
 from tenacity import (  # for exponential backoff
@@ -10,19 +10,37 @@ from tenacity import (  # for exponential backoff
     wait_random_exponential,
 )
 
-
-
-
-
 class OpenAIService:
-    CODE_REVIEW_SYSTEM_PROMPT = """You are a world-class software engineer. When provided a code snippet, you analyze it and offer any code suggestions to improve it. If you have suggestions, you present them in the following GitHub suggestion format:
-```suggestion
--[Code you want to remove]
-+[Your code suggestion here]
-```
-If no improvements or suggestions are identified,  write "No suggestions.". Your suggestions are relevant and actionable, considering the provided context."""
+    CODE_REVIEW_SYSTEM_PROMPT_OLD = """You are a helpful and funny world-class software engineer. When provided a git diffs, you analyze it and if there are improvements you can make, you detail the improvements in 3 or less bullets. Your suggestions are actionable, not stylistic. If you cannot make suggestions, you respond with "No suggestions.". The suggestions should not include stylistic improvements like adding typehints or docstrings. Your suggestions are relevant and actionable, considering the provided context."""
+    CODE_REVIEW_SYSTEM_PROMPT_OLD = """You are a helpful and funny world-class software engineer. You analyze provided code and you detail the improvements using the following guidelines:
+- Provide Constructive Feedback: Be kind and respectful. Frame feedback as suggestions and use questions to provoke thought rather than making authoritative statements. 
+- Be concise, constructive, and respectful. Recognize good work. 
+- Do not suggest typehint, type hint, docstring, or comment additions.
+- Do not make assumptions about the code. Ask questions if anything is unclear.
+- Only focus on logical errors, potential bugs, or vulnerabilities introduced by the changes. Consider edge cases and corner scenarios that might not have been accounted for in the initial implementation.
+- Avoid Nitpicking. Only focus on significant issues.
+- Feedback must be relevant and actionable.
+- Output a max of 3 suggestions.
+- Review comments should be well organized and easy to read.
+If you cannot make suggestions, you respond with "No suggestions."."""
 
-    PR_REVIEW_COMMENT_SYSTEM_PROMPT = """You are a world-class software engineer and writer. Given a list of PR review comments, you provide a summary of the comments. If there are no comments, write "No comments.". Your summary is relevant and actionable, considering the provided context."""
+    CODE_REVIEW_SYSTEM_PROMPT = """You are a helpful and funny world-class code reviewer. You provide funny and insightful feedback on the code changes given. You analyze provided code & diffs, and you detail the improvements using the following guidelines:"""
+
+    SUMMARIZE_CODE_REVIEW_SYSTEM_PROMPT = """You are a helpful and funny world-class technical writer. You analyze descriptions of suggested code updates and summarize the most important ones using the following guidelines:
+- Provide Constructive Feedback: Be kind and respectful. Frame feedback as suggestions and use questions to provoke thought rather than making authoritative statements. 
+- Be concise, constructive, and respectful. Recognize good work. 
+- Do not suggest typehint, type hint, docstring, or comment additions.
+- Do not make assumptions about the code.  Ask questions if anything is unclear.
+- Only focus on logical errors, potential bugs, or vulnerabilities introduced by the changes. Consider edge cases and corner scenarios that might not have been accounted for in the initial implementation.
+- Avoid Nitpicking. Only focus on significant issues.
+- Feedback must be relevant and actionable.
+- Output a max of 3 suggestions.
+- Review comments should be well organized and easy to read.
+Structure and organize your response using markdown and emojis. If there is nothing to summarize, you respond with "No suggestions."."""
+
+    CODE_SUGGESTION_SYSTEM_PROMPT = """You are a world-class software engineer. When provided a code snippet, you analyze it and if there are improvements you can make, you respond with the improved code diff. If there aren't impromvents you can make, you respond with "No suggestions." ONLY."""
+
+    PR_REVIEW_COMMENT_SYSTEM_PROMPT = """You are a helpful and funny world-class software engineer. Given a list of code suggestions, you respond with a summary of the suggestions. You format your suggestions in easy to read markdown. If there are no comments, write "No comments.". Your summary is relevant and actionable, considering the provided context. You use emojis where appropriate."""
 
     def __init__(self, openai_api_key: str):
         openai.api_key = openai_api_key
@@ -30,19 +48,26 @@ If no improvements or suggestions are identified,  write "No suggestions.". Your
 
     @retry(wait=wait_random_exponential(min=0.2, max=60), stop=stop_after_attempt(6))
     def analyze(
-        self, diff: str, system_prompt: str = CODE_REVIEW_SYSTEM_PROMPT
+        self, diff: str, content: str
     ):
-        query = f"""Review the code below and provide code suggestions.  If there are no improvements or suggestions, write "No suggestions.".
-        ```
-        {diff}
-        ```
-        """
+        query = f"""Your task is:
+- Review the code changes and provide feedback.
+- Highlight bugs them.
+- Provide details on missed use of best-practices.
+- Do not highlight minor issues and nitpicks.
+- Use bullet points if you have multiple comments.
+- Provide security recommendations if there are any.
+- If there are no suggestions, write "No suggestions."
+Full file:
+{content}
+Diff to review:
+{diff}"""
 
         response = openai.ChatCompletion.create(
             messages=[
                 {
                     "role": "system",
-                    "content": system_prompt,
+                    "content": self.CODE_REVIEW_SYSTEM_PROMPT,
                 },
                 {"role": "user", "content": query},
             ],
@@ -50,20 +75,44 @@ If no improvements or suggestions are identified,  write "No suggestions.". Your
             temperature=0,
         )
         return response.choices[0]["message"]["content"]
-
-
     @retry(wait=wait_random_exponential(min=0.2, max=60), stop=stop_after_attempt(6))
-    def summarize(
-        self, comments: [str], system_prompt: str = CODE_REVIEW_SYSTEM_PROMPT
+    def code_suggestions(
+        self, diff: str, text_suggestions: str
     ):
+        query = f"""Based on the code recommendations listed, output code suggestions in the unified format diff.
+Code recommendations:
+{text_suggestions}
+Code to review:
+{diff}"""
 
-        query = "\n* ".join(comments)
+        print(query)
 
         response = openai.ChatCompletion.create(
             messages=[
                 {
                     "role": "system",
-                    "content": system_prompt,
+                    "content": self.CODE_SUGGESTION_SYSTEM_PROMPT,
+                },
+                {"role": "user", "content": query},
+            ],
+            model=self.GPT_MODEL,
+            temperature=1,
+        )
+        return response.choices[0]["message"]["content"]
+
+
+    @retry(wait=wait_random_exponential(min=0.2, max=60), stop=stop_after_attempt(6))
+    def summarize(
+        self, comments: Dict[str, str]
+    ):
+        query = "\n".join([f"\nFile: {k}\n{v}" for k, v in comments.items()])
+        print(query)
+
+        response = openai.ChatCompletion.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": self.SUMMARIZE_CODE_REVIEW_SYSTEM_PROMPT,
                 },
                 {"role": "user", "content": query},
             ],
